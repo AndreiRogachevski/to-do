473b77b20b7c9c9a2300b5e177d1c980606c416f
AndreiRogachevski <ARogov89@gmail.com>
2023-04-03 00:28:13 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Form from './Form';
+import List from './List';
 
 function App() {
     return (
       <div className="App">
       <Form/>
+      <List/>
       </div>
     );
 }
--- src/List.js
+++ src/List.js
@@ -1,3 +1,13 @@
+import React,{ useEffect, useState } from "react"
+
 export default function List() {
-  return <ul></ul>
+  const [list, setList] = useState([])
+  useEffect(() => {
+    fetch('https://dummyjson.com/todos')
+    .then(res => res.json())
+    .then(data => setList(data.todos))
+  }, [])
+  return <ul>{
+    list.map(item => <li key={item.id}>{item.todo}</li>)
+  }</ul>
 };
